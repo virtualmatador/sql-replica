1827f0eebf7a85c15a01df6e86823bcde736e1ca
Ali Asadpoor <virtualmatador@yahoo.com>
2022-01-20 00:11:46 -0500

    Allow dry run

--- main.cpp
+++ main.cpp
@@ -6,71 +6,62 @@
 #include <json.h>
 #include <sqlr.h>
 
-void version()
-{
-    std::cout << "SQL Replica, Version: " << VERSION << std::endl;
-}
-
-jsonio::json read_json(const std::string& file_name)
-{
-    std::ifstream the_file{ file_name };
-    if (!the_file)
-    {
-        throw std::runtime_error("Cannot Read File");
-    }
-    jsonio::json the_json;
-    the_file >> the_json;
-    if (!the_json.completed())
-    {
-        throw std::runtime_error("Bad Json");
-    }
-    return the_json;
-}
-
-void convert(const std::string& db_name,
-    const std::string& sql_file_name, const std::string& clients_file_name)
-{
-    jsonio::json sql_json = read_json(sql_file_name);
-    jsonio::json clients_json;
-    if (!clients_file_name.empty())
-    {
-        clients_json = read_json(clients_file_name);
-    }
-    else
-    {
-        std::istringstream("[]") >> clients_json;
-    }
-    std::cout << replicate_sql(db_name, sql_json, clients_json) << std::endl;
-}
-
 int main(int argc, const char* argv[])
 {
+    std::string name;
+    jsonio::json db, clients = jsonio::json_array<jsonio::json>();
+    jsonio::json report = false;
+    bool convert = false;
     try
     {
         Cli::parse(argc, argv,
         {
             {
-                "--version",
+                { "--version", "-v" },
                 Cli::Handler({ [&](const std::vector<std::string>& args)
                 {
-                    version();
+                    std::cout << "SQL Replica, Version: " << VERSION <<
+                        std::endl;
                 }, 0, 0})
             },
             {
-                "-v",
+                { "--report", "-r" },
                 Cli::Handler({ [&](const std::vector<std::string>& args)
                 {
-                    version();
-                }, 0, 0})
+                    std::istringstream(args[0]) >> report;
+                    convert = true;
+                }, 1, 1})
             },
             {
-                "",
+                { "--name", "-n" },
                 Cli::Handler({ [&](const std::vector<std::string>& args)
                 {
-                    convert(args[0], args[1], args.size() > 2 ? args[2] : "");
-                }, 2, 3})
+                    name = args[0];
+                    convert = true;
+                }, 1, 1})
+            },
+            {
+                { "--clients", "-c" },
+                Cli::Handler({ [&](const std::vector<std::string>& args)
+                {
+                    std::ifstream(args[0]) >> clients;
+                    convert = true;
+                }, 1, 1})
+            },
+            {
+                { "" },
+                Cli::Handler({ [&](const std::vector<std::string>& args)
+                {
+                    std::ifstream(args[0]) >> db;
+                    convert = true;
+                }, 1, 1})
             }
         });
+        if (convert)
+        {
+            std::cout << replicate_sql(
+                report.get_bool(), name, db, clients) << std::endl;
+        }
     }
     catch(const std::exception& e)
     {
