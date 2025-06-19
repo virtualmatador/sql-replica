867002bda312538c236b0b2fe869e14a5102f2a4
Ali Asadpoor <virtualmatador@yahoo.com>
2025-06-19 14:24:07 -0400

    Reflect failure on exit code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -6,117 +7,95 @@
 #include <json.h>
 #include <sqlr.h>
 
-int main(int argc, const char* argv[])
-{
-    std::string name;
-    jsonio::json_arr db, clients;
-    jsonio::json report = false;
-    std::string output_file;
-    bool convert = false;
-    std::vector<std::string> errors;
-    try
-    {
-        Cli::parse(argc, argv,
-        {
-            {
-                { "--version", "-v" },
-                Cli::Handler({ [&](const std::vector<std::string>& args)
-                {
-                    std::cout << "SQL Replica, Version: " << VERSION <<
-                        std::endl;
-                }, 0, 0})
-            },
-            {
-                { "--report", "-r" },
-                Cli::Handler({ [&](const std::vector<std::string>& args)
-                {
-                    std::istringstream(args[0]) >> report;
-                    if (report.type() != jsonio::JsonType::J_BOOL)
-                    {
-                        errors.emplace_back(
-                            "Report flag is not valid: " + args[0]);
-                    }
-                    convert = true;
-                }, 1, 1})
-            },
-            {
-                { "--name", "-n" },
-                Cli::Handler({ [&](const std::vector<std::string>& args)
-                {
-                    name = args[0];
-                    convert = true;
-                }, 1, 1})
-            },
-            {
-                { "--client", "-c" },
-                Cli::Handler({ [&](const std::vector<std::string>& args)
-                {
-                    jsonio::json_obj client;
-                    client["user"] = args[0];
-                    jsonio::json_arr permissions;
-                    std::ifstream(args[1]) >> permissions;
-                    if (!permissions.completed())
-                    {
-                        errors.emplace_back(
-                            "Permissions file is not valid: " + args[1]);
-                    }
-                    client["permissions"] = std::move(permissions);
-                    clients.emplace_back(std::move(client));
-                    convert = true;
-                }, 2, 2})
-            },
-            {
-                { "--out", "-o" },
-                Cli::Handler({ [&](const std::vector<std::string>& args)
-                {
-                    output_file = args[0];
-                    convert = true;
-                }, 1, 1})
-            },
-            {
-                { "" },
-                Cli::Handler({ [&](const std::vector<std::string>& args)
-                {
-                    std::ifstream(args[0]) >> db;
-                    if (!db.completed())
-                    {
-                        errors.emplace_back(
-                            "Database file is not valid: " + args[0]);
-                    }
-                    convert = true;
-                }, 1, 1})
-            }
-        });
-        if (errors.empty())
-        {
-            if (convert)
-            {
-                std::ostream* os;
-                std::ofstream ofs;
-                if (output_file.empty())
-                {
-                    os = &std::cout;
-                }
-                else
-                {
-                    ofs.open(output_file);
-                    os = &ofs;
-                }
-                (*os) << replicate_sql(report.get_bool(), name, db, clients) <<
-                    std::endl;
-            }
+int main(int argc, const char *argv[]) {
+  std::string name;
+  jsonio::json_arr db, clients;
+  jsonio::json report = false;
+  std::string output_file;
+  bool convert = false;
+  std::vector<std::string> errors;
+  try {
+    Cli::parse(
+        argc, argv,
+        {{{"--version", "-v"},
+          Cli::Handler({[&](const std::vector<std::string> &args) {
+                          std::cout << "SQL Replica, Version: " << VERSION
+                                    << std::endl;
+                        },
+                        0, 0})},
+         {{"--report", "-r"},
+          Cli::Handler({[&](const std::vector<std::string> &args) {
+                          std::istringstream(args[0]) >> report;
+                          if (report.type() != jsonio::JsonType::J_BOOL) {
+                            errors.emplace_back("Report flag is not valid: " +
+                                                args[0]);
+                          }
+                          convert = true;
+                        },
+                        1, 1})},
+         {{"--name", "-n"},
+          Cli::Handler({[&](const std::vector<std::string> &args) {
+                          name = args[0];
+                          convert = true;
+                        },
+                        1, 1})},
+         {{"--client", "-c"},
+          Cli::Handler({[&](const std::vector<std::string> &args) {
+                          jsonio::json_obj client;
+                          client["user"] = args[0];
+                          jsonio::json_arr permissions;
+                          std::ifstream(args[1]) >> permissions;
+                          if (!permissions.completed()) {
+                            errors.emplace_back(
+                                "Permissions file is not valid: " + args[1]);
+                          }
+                          client["permissions"] = std::move(permissions);
+                          clients.emplace_back(std::move(client));
+                          convert = true;
+                        },
+                        2, 2})},
+         {{"--out", "-o"},
+          Cli::Handler({[&](const std::vector<std::string> &args) {
+                          output_file = args[0];
+                          convert = true;
+                        },
+                        1, 1})},
+         {{""},
+          Cli::Handler({[&](const std::vector<std::string> &args) {
+                          std::ifstream(args[0]) >> db;
+                          if (db.completed()) {
+                            if (name.empty()) {
+                              name = std::filesystem::path{args[0]}.stem();
+                            }
+                            convert = true;
+                          } else {
+                            errors.emplace_back("Database file is not valid: " +
+                                                args[0]);
+                          }
+                        },
+                        1, 1})}});
+    if (errors.empty()) {
+      if (convert) {
+        std::ostream *os;
+        std::ofstream ofs;
+        if (output_file.empty()) {
+          os = &std::cout;
+        } else {
+          ofs.open(output_file);
+          os = &ofs;
         }
-        else
-        {
-            for (const auto& error : errors)
-            {
-                std::cerr << error << std::endl;
-            }
-        }
+        (*os) << replicate_sql(report.get_bool(), name, db, clients)
+              << std::endl;
+      }
+    } else {
+      for (const auto &error : errors) {
+        std::cerr << error << std::endl;
+      }
+      return EXIT_FAILURE;
     }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    return 0;
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
